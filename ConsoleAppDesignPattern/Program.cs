16b65161f4918b50947b6fe9042b8d4d46b0500e
admin <komalkanawade11@gmail.com>
2024-10-13 13:03:55 +0530

    Add project files.

---
+++ ConsoleAppDesignPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleAppDesignPattern
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton s1 = Singleton.GetInstance();
+
+            //Singleton s2 = Singleton.GetInstance();
+
+            //if (s1 != s2)
+            //{
+            //    Console.WriteLine("Not shering the same resource");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("Shering the same resorce");
+            //}
+            //Console.ReadLine();
+
+            Singleton s1 = Singleton.GetInstance();
+            s1.getDatabaseObject().StringSet("persone", "Claverperson");
+
+            Singleton s2 = Singleton.GetInstance();
+
+            Console.WriteLine(s2.getDatabaseObject().StringGet("person"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleAppDesignPattern/Singleton.cs
@@ -0,0 +1,43 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDesignPattern
+{
+    internal class Singleton
+    {
+        // declare private static field
+        public static Singleton instance;
+
+        private static ConnectionMultiplexer con;
+
+        private static IDatabase db;
+
+        // private constructor
+        private Singleton()
+        {
+            con = ConnectionMultiplexer.Connect("localhost");
+        }
+
+        // GetInstance method which will be be accessed from outside scope
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        // Any business logic needed
+       public IDatabase getDatabaseObject()
+        {
+            db = con.GetDatabase();
+            return db;
+        }
+    }
+}
